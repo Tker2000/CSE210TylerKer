0cb084778fff610fb2d233cda5369433a515d58f
kert <ker21012@byui.edu>
2025-01-16 19:33:30 -0700

    Prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
-class Program
+class NumberGuesser
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rand = new Random();
+        int magicNumber = rand.Next(1, 101);
+
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
+class NumberList
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        int number;
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+        
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int largest = numbers.Max();
+        
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
+
+        int smallestPositive = numbers.Where(n => n > 0).Min();
+        List<int> sortedNumbers = numbers.OrderBy(n => n).ToList();
+        
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        
+        Console.WriteLine("The sorted list is:");
+        foreach (var num in sortedNumbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
