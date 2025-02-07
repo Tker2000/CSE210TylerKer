2393102676466ed6c8cb7e28eea7da86fbfffad6
kert <ker21012@byui.edu>
2025-02-06 20:12:26 -0700

    Prepare W05

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,65 @@
 using System;
 
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
+    private int _numerator;
+    private int _denominator;
+
+    // Constructors
+    public Fraction()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    // Getters and Setters
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+
+    public void SetDenominator(int denominator)
+    {
+        if (denominator != 0)
+        {
+            _denominator = denominator;
+        }
+        else
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+    }
+
+    // Methods
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
     }
 }
